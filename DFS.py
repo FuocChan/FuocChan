45ff49a9de933bb74d1e2afa7bbab285965e26fc
FuocChan <102860036+FuocChan@users.noreply.github.com>
2022-07-11 10:03:37 +0700

    Add files via upload

--- DFS.py
+++ DFS.py
@@ -1,26 +1,3 @@
-'''
-intput
-9
-9
-1
-2
-1
-3
-1
-5
-2
-4
-3
-6
-3
-7
-3
-9
-5
-8
-8
-9
-'''
 def dfs(u):
     loc=[]
     DinhKe=[]
@@ -40,11 +17,39 @@ def dfs(u):
                 dfs(v)
 
 aV,aE=int(input()),int(input())
-xuat=[]
 A=[]
 visited=['F']*aV
+cnt=0
+
 for i in range(0,aE):
     x,y=int(input()),int(input())
     A.append([x,y])
-dfs(1)
-print(xuat)
+for i in range(1,aV+1):
+    xuat=[]
+    if visited[i-1]=='F':
+        cnt+=1
+        dfs(i)
+        print(xuat)
+print('số liên thông:', cnt)
+
+'''
+intput
+10
+8
+1
+2
+2
+3
+2
+4
+3
+6
+3
+7
+6
+7
+5
+8
+8
+9
+'''
